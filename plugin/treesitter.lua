6191085f039e726a147040e1431d7a3aa2fd4b67
Mathias <mathias.e.karlsen@gmail.com>
2026-08-25 14:51:37 +0200

    configured treesitter

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,3 +1,4 @@
+-- Automatically update parsers to the latest version when upgrading the plugin.
 vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
   local name, kind = ev.data.spec.name, ev.data.kind
   if name == 'nvim-treesitter' and kind == 'update' then
@@ -8,4 +9,32 @@ end })
 
 vim.pack.add({
   'https://github.com/nvim-treesitter/nvim-treesitter',
+})
+
+local parsers = {
+  "lua",
+  "python",
+  "c",
+  "cpp",
+  "cmake",
+  "bash",
+  "javascript",
+  "html",
+  "css",
+  "json",
+  "markdown",
+  "markdown_inline",
+  "dockerfile",
+}
+
+require("nvim-treesitter").install(parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+
+    if lang and vim.tbl_contains(parsers, lang) then
+      pcall(vim.treesitter.start)
+    end
+  end,
 })
